0ee8af12cfbde6bf37c913fa77fc57336276a8bc
Fabrício Brito <fab.brito94@gmail.com>
2018-12-26 11:22:23 -0300

    Working remotely

--- spy_fi/ape.py
+++ spy_fi/ape.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-import telnetlib
+from pwn import *
+import string
 
 
 def parse_blocks(x, n):
@@ -10,14 +11,13 @@ def parse_blocks(x, n):
     return blocks
 
 
-def tnconn(data):
-    tn = telnetlib.Telnet("2018shell.picoctf.com", 33893)
-    tn.read_until("Please enter your situation report: ")
-    send_string = str(data.decode('ascii') + "\r\n")
-    tn.write(send_string)
-    challenge = tn.read_all()
-    tn.close()
-    return challenge
+def pwn_conn(payload):
+    context.log_level = 'critical'
+    sh = remote('2018shell2.picoctf.com', 33893)
+    sh.sendlineafter(': ', payload)
+    data = sh.recvall()
+    sh.close()
+    return parse_blocks(data[:-1], 32)
 
 
 def padit(message):
@@ -30,31 +30,31 @@ def main():
     text0 = "Agent,\nGreetings. My situation report is as follows:\n"  # 53
     text1 = "\nMy agent identifying code is: "  # 31
     text2 = ".\nDown with the Soviets,\n006\n"  # 29
-    # print(len(text0), len(text1), len(text2))
-    leak_len = 16
+    leak_len = 40
 
     offset = 16 - len(text0) % 16
-    guessed_secret = ''
+    guessed_secret = 'picoCTF{@g3nt6_1$_th3_c00l3$t_'
 
-    for i in range(0, leak_len):
+    for i in range(len(guessed_secret), leak_len):
         payload = bytearray(text1[-15:] + guessed_secret)
         payload.append('?')
-        for guess in range(0x20, 0x7F):
-            payload[-1] = chr(guess)
+        for guess in string.printable:
+            payload[-1] = guess
             pfix = 16 - i % 16
             data = bytearray('0' * offset + payload[-16:] + '0' * pfix)
-            res = tnconn(data)
-            c_blocks = parse_blocks(res[:-1], 32)
+            c_blocks = pwn_conn(data)
             chosen_block = c_blocks[4]
             tail = "".join(c_blocks[5:])
             print "> {0} --- {1} --- {2} --- {3}".format(data[offset: offset + 16],
                                                          chosen_block, (chosen_block in tail), guessed_secret)
             if chosen_block in tail:
-                guessed_secret += chr(guess)
+                guessed_secret += guess
                 break
         if guessed_secret == '':
             print '########### ERROR ###########'
             return
+        if guessed_secret[-1] == '}':
+            break
     print "All guessed bytes: " + guessed_secret
 
 
--- spy_fi/local_ape.py
+++ spy_fi/local_ape.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
 from Crypto.Cipher import AES
-
+import string
 
 def sim_conn(sitrep):
-    agent_code = "picoCTF{__THIS_IS_BULLSHIT__}"
+    agent_code = "picoCTF{THIS_IS_SUCH_BULLSHIT_8024702874}"
     plain = """Agent,\nGreetings. My situation report is as follows:\n{0}\nMy agent identifying code is: {1}.\nDown with the Soviets,\n006\n\n""".format(
         sitrep, agent_code)
     plain = padit(plain)
@@ -31,18 +31,19 @@ def main():
     text1 = "\nMy agent identifying code is: "  # 31
     text2 = ".\nDown with the Soviets,\n006\n"  # 29
     # print(len(text0), len(text1), len(text2))
-    leak_len = 32
+    leak_len = 48
 
     offset = 16 - len(text0) % 16
-    guessed_secret = ''
+    guessed_secret = 'picoCTF'
 
     for i in range(0, leak_len):
         payload = bytearray(text1[-15:] + guessed_secret)
         payload.append('?')
-        for guess in range(0x20, 0x7F):
-            payload[-1] = chr(guess)
+        for guess in string.printable:
+            payload[-1] = guess
             pfix = 16 - i % 16
-            data = bytearray('0' * offset + payload[-16:] + '0' * pfix)
+            data = bytearray('0' * offset +
+                             payload[-16:] + '0' * pfix)
             res = sim_conn(data)
             c_blocks = parse_blocks(res[:-1], 32)
             chosen_block = c_blocks[4]
@@ -50,11 +51,13 @@ def main():
             print "> {0} --- {1} --- {2} --- {3}".format(data[offset: offset + 16],
                                                          chosen_block, (chosen_block in tail), guessed_secret)
             if chosen_block in tail:
-                guessed_secret += chr(guess)
+                guessed_secret += guess
                 break
         if guessed_secret == '':
             print '########### ERROR ###########'
             return
+        if guessed_secret[-1] == '}':
+            break
     print "All guessed bytes: " + guessed_secret
 
 
